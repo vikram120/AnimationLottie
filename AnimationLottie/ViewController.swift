30f916f6cf1c499345e124f7b6cb076b124c2c42
vikram <vikramkunwar1404@gmail.com>
2022-10-03 16:40:04 +0530

    car Animation

--- AnimationLottie/ViewController.swift
+++ AnimationLottie/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    let animationView = AnimationView()
 
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setupAnimation()
+        }
+        
+        private func setupAnimation(){
+            animationView.animation = Animation.named("LottieCar")
+            animationView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+            animationView.center = view.center
+            animationView.contentMode = .scaleAspectFit
+            animationView.loopMode = .loop
+            animationView.play()
+            view.addSubview(animationView)
+        }
 
 }
 
