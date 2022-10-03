09534acb32a2d907c36d93728aa11da57dcf312c
vikram <vikramkunwar1404@gmail.com>
2022-10-03 14:24:24 +0530

    Initial Commit

---
+++ AnimationLottie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationLottie
+//
+//  Created by Vikram Kunwar on 03/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
